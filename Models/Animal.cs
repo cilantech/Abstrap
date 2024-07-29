4b44cdc6180c8ae01ab3f35aa794e6cebdd69f61
cilantech <isazaherbin@gmail.com>
2024-07-29 07:05:52 -0500

    feat: Mejora de clase animal volviendo propiedades y metodos protecte

--- Models/Animal.cs
+++ Models/Animal.cs
@@ -7,19 +7,19 @@ namespace Abstrap.Models;
 
 public class Animal
 {
-    public int Id { get; set; }
+    protected int Id { get; set; }
 
-    public string Nombre { get; set; }
-    public string Especie { get; set; }
+    protected string Nombre { get; set; }
+    protected string Especie { get; set; }
 
-    public Animal(int id, string nombre, string especie)
+    protected Animal(int id, string nombre, string especie)
     {
         Id = id;
         Nombre = nombre;
         Especie = especie;
     }
 
-    public void saludar(){
+    protected void saludar(){
         Console.WriteLine($"Hola, soy {Nombre}");    }
 
 }
